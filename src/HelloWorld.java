829d5e1ac3ced93ecd124f7d085888d3c4c1f15b
Zachary Robbins <zrrobbins@wpi.edu>
2014-11-11 21:28:26 -0500

    Created initial project

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("This is me testing to see if git works");
+	}
+}
+
