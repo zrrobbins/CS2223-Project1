5d3553c4a67079111a57673c6ea9ba38af160e05
Zachary Robbins <zrrobbins@wpi.edu>
2014-11-11 22:05:42 -0500

    Added Launcher class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public void sayHello(){
 		System.out.println("Hello World!");
 		System.out.println("This is me testing to see if git works");
 	}
